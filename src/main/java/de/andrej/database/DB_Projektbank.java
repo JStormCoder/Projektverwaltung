982163dc940911d5e1e1aaafc0b44eeab96e5a6c
RA <anru@mail.de>
2024-02-01 21:38:32 +0100

    Datenbank erstellt Verbindung mit Klasse aufgebaut

---
+++ src/main/java/de/andrej/database/DB_Projektbank.java
@@ -0,0 +1,31 @@
+package de.andrej.database;
+
+import java.sql.*;
+
+public class DB_Projektbank {
+
+    public static final String DB_NAME = "Projektebank.sqlite";
+    public static final String DB_PATH = "D:\\Andrej\\Projekte\\JavaProjekte\\Projektverwaltung";
+    public static final String CONNECTION_STRING = "jdbc:sqlite:" + DB_PATH + DB_NAME;
+
+    private Connection connection;
+    private Statement statement;
+
+    public boolean open() {
+        try {
+            connection = DriverManager.getConnection(CONNECTION_STRING);
+            statement = connection.createStatement();
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public Statement getStatement() {
+        return statement;
+    }
+}
