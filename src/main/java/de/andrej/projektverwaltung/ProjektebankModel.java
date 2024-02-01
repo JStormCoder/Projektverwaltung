232eee1d541418684ad25538d49f1aa1f9821383
RA <anru@mail.de>
2024-02-01 21:50:46 +0100

    Datenbank erstellt Verbindung mit Klasse aufgebaut

---
+++ src/main/java/de/andrej/projektverwaltung/ProjektebankModel.java
@@ -0,0 +1,13 @@
+package de.andrej.projektverwaltung;
+
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class ProjektebankModel {
+    private final String TABLE_NAME = "Projekte";
+
+    private final String QUERY_DATA_FROM_PROJEKTE_TABLE = "";
+
+
+}
