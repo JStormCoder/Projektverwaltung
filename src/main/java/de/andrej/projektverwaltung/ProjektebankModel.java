48b3b4bcdb7d712b7e9f885d5c066dfa01ae31cc
JStormCoder <157954853+JStormCoder@users.noreply.github.com>
2024-02-02 06:46:57 +0100

    ProjektebankModel.java aktualisieren

--- src/main/java/de/andrej/projektverwaltung/ProjektebankModel.java
+++ src/main/java/de/andrej/projektverwaltung/ProjektebankModel.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 public class ProjektebankModel {
     private final String TABLE_NAME = "Projekte";
 
-    private final String QUERY_DATA_FROM_PROJEKTE_TABLE = "";
+    private final String QUERY_DATA_FROM_PROJEKTE_TABLE = "SElect * From " + TABLE_NAME;
 
 
 }
